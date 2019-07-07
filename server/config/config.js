dd729b2521b9c9be7460143863dc98c78dc818d4
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-07 17:34:40 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-  urlDB = 'mongodb+srv://developer:developer2019@cluster0-klay2.mongodb.net/cafe?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
